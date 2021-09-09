cfb30d17ffaa7025d10ed5b2898021b401b987c6
Crowdbotics <team@crowdbotics.com>
2021-09-09 18:51:48 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "rapid_wood_30420Identifier",
+  url: "https://rapid_wood_30420Identifier.botics.co",
+  api: "https://rapid_wood_30420Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
